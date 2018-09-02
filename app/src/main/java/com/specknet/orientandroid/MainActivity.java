576f82737c6474dd79c92e02c06c84f9ac8706b9
rat <rat@speckled>
2018-09-02 14:15:31 +0100

    Add rxandroidble2

--- app/src/main/java/com/specknet/orientandroid/MainActivity.java
+++ app/src/main/java/com/specknet/orientandroid/MainActivity.java
@@ -2,12 +2,73 @@ package com.specknet.orientandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.polidea.rxandroidble2.RxBleClient;
+import com.polidea.rxandroidble2.RxBleDevice;
+import com.polidea.rxandroidble2.scan.ScanSettings;
+
+import java.util.UUID;
+
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends Activity {
+
+    private final String ORIENT_BLE_ADDRESS = "00:11:22:33:44:55";
+    private static final String ORIENT_CHARACTERISTIC = "0934-234";
+    private RxBleDevice orient_device;
+    private Disposable scanSubscription;
+    private RxBleClient rxBleClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rxBleClient = RxBleClient.create(this);
+
+        scanSubscription = rxBleClient.scanBleDevices(
+                new ScanSettings.Builder()
+                        // .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY) // change if needed
+                        // .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES) // change if needed
+                        .build()
+                // add filters if needed
+        )
+                .subscribe(
+                        scanResult -> {
+                            Log.i("OrientAndroid", "FOUND: " + scanResult.getBleDevice().getName() + ", " +
+                                    scanResult.getBleDevice().getMacAddress());
+                            // Process scan result here.
+                            if (scanResult.getBleDevice().getMacAddress() == ORIENT_BLE_ADDRESS) {
+                                connectToOrient(ORIENT_BLE_ADDRESS);
+                                scanSubscription.dispose();
+                            }
+                        },
+                        throwable -> {
+                            // Handle an error here.
+                        }
+                );
+
+    }
+    private void connectToOrient(String addr) {
+        orient_device = rxBleClient.getBleDevice(addr);
+
+        orient_device.establishConnection(false)
+                .flatMap(rxBleConnection -> rxBleConnection.setupNotification(UUID.fromString(ORIENT_CHARACTERISTIC)))
+                .doOnNext(notificationObservable -> {
+                    // Notification has been set up
+                })
+                .flatMap(notificationObservable -> notificationObservable) // <-- Notification has been set up, now observe value changes.
+                .subscribe(
+                        bytes -> {
+                            // Given characteristic has been changes, here is the value.
+                            Log.i("OrientAndroid", "Received " + bytes.length + " bytes");
+                        },
+                        throwable -> {
+                            // Handle an error here.
+                        }
+                );
+
+
     }
 }
