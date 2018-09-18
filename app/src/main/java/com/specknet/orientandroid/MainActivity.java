0b15dc0a9e980f931f69ad01cc2e0a341b7c55f0
andrewbates11 <andrew.bates11@gmail.com>
2018-09-18 23:14:46 +0100

    working

--- app/src/main/java/com/specknet/orientandroid/MainActivity.java
+++ app/src/main/java/com/specknet/orientandroid/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -65,12 +68,29 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     private TextView captureTimetextView;
     private TextView accelTextView;
     private TextView gyroTextView;
+    private EditText nameEditText;
+    private EditText notesEditText;
 
     private Spinner positionSpinner;
     private Spinner groupSpinner;
+    private Spinner activitySpinner;
 
-    private String group = null;
-    private String position = null;
+    private String group_str = null;
+    private String position_str = null;
+    private String act_type_str = null;
+    private String name_str = null;
+    private String notes_str = null;
+    private String side_str = null;
+
+    private String orientation_str = null;
+    private String steps_str = null;
+    private String mounting_str = null;
+
+    private RadioGroup sideRadioGroup;
+    private RadioGroup orientationRadioGroup;
+    private RadioGroup stepsRadioGroup;
+    private RadioGroup mountingRadioGroup;
+
 
 
     @Override
@@ -88,12 +108,21 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         gyroTextView = findViewById(R.id.gyroTextView);
         positionSpinner = findViewById(R.id.positionSpinner);
         groupSpinner = findViewById(R.id.groupSpinner);
+        activitySpinner = findViewById(R.id.activitySpinner);
+        nameEditText = findViewById(R.id.nameEditText);
+        notesEditText = findViewById(R.id.notesEditText);
 
         positionSpinner.setOnItemSelectedListener(this);
         groupSpinner.setOnItemSelectedListener(this);
+        activitySpinner.setOnItemSelectedListener(this);
+
+        sideRadioGroup = findViewById(R.id.radioSide);
+        orientationRadioGroup = findViewById(R.id.radioSide);
+        stepsRadioGroup = findViewById(R.id.radioSteps);
+        mountingRadioGroup = findViewById(R.id.radioMouting);
 
         List<String> position_list = new ArrayList<String>();
-        position_list.add("POSITION");
+        position_list.add("---");
         position_list.add("Wrist");
         position_list.add("Upper arm");
         position_list.add("Torso");
@@ -108,16 +137,25 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         positionSpinner.setAdapter(adapter);
 
         List<String> group_list = new ArrayList<String>();
-        group_list.add("GROUP");
+        group_list.add("---");
         for (char c = 'A'; c <= 'Z'; c++){
             group_list.add(String.valueOf(c));
         }
 
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, group_list);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         groupSpinner.setAdapter(adapter2);
 
+        List<String> activity_list = new ArrayList<String>();
+        activity_list.add("---");
+        activity_list.add("Walking (level ground)");
+        activity_list.add("Climbing stairs");
+        activity_list.add("Descending stairs");
+        activity_list.add("Running");
 
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, activity_list);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        activitySpinner.setAdapter(adapter3);
 
         try {
             FileInputStream fis = new FileInputStream(path + "/" + "orient.ble");
@@ -132,31 +170,90 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         }
 
         start_button.setOnClickListener(v-> {
-            start_button.setEnabled(false);
-            // make a new filename based on the start timestamp
-            String file_ts = new SimpleDateFormat("yyyyMMdd'T'HHmmss").format(new Date());
-            file = new File(path, "PdIoT_" + file_ts + ".csv");
 
-            try {
-                writer = new CSVWriter(new FileWriter(file), ',');
-            } catch (IOException e) {
-                Log.e("MainActivity", "Caught IOException: " + e.getMessage());
+            if (group_str == null || act_type_str == null || position_str == null || (nameEditText.getText().toString().length() == 0)) {
+                Toast.makeText(this,"Please complete all details",
+                        Toast.LENGTH_SHORT).show();
             }
+            else {
+                start_button.setEnabled(false);
 
-            String[] entries = "timestamp#seq#accel_x#accel_y#accel_z#gyro_x#gyro_y#gyro_z".split("#");
-            writer.writeNext(entries);
-            try {
-                writer.flush();
-            } catch (IOException e) {
-                Log.e("MainActivity", "Caught IOException: " + e.getMessage());
+                name_str = nameEditText.getText().toString();
+                notes_str = notesEditText.getText().toString();
+
+                int selectedId;
+                RadioButton rb;
+
+                selectedId = sideRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                side_str = rb.getText().toString();
+
+                selectedId = orientationRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                orientation_str = rb.getText().toString();
+
+                selectedId = stepsRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                steps_str = rb.getText().toString();
+
+                selectedId = mountingRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                mounting_str = rb.getText().toString();
+
+
+                // make a new filename based on the start timestamp
+                String file_ts = new SimpleDateFormat("yyyyMMdd'T'HHmmss").format(new Date());
+                file = new File(path, "PDIoT_" + group_str + "_" + file_ts + ".csv");
+
+                try {
+                    writer = new CSVWriter(new FileWriter(file), ',');
+                } catch (IOException e) {
+                    Log.e("MainActivity", "Caught IOException: " + e.getMessage());
+                }
+
+                String[] h1 = {"# PDIoT walking data"};
+                writer.writeNext(h1);
+
+                String[] h2 = {"# Start time: " + file_ts};
+                writer.writeNext(h2);
+
+                String[] h3 = {"# Group: " + group_str};
+                writer.writeNext(h3);
+
+                String[] h4 = {"# Subject: " + name_str};
+                writer.writeNext(h4);
+
+                String[] h5 = {"# Activity type: " + act_type_str};
+                writer.writeNext(h5);
+
+                String[] h6 = {"# Sensor position: " + position_str};
+                writer.writeNext(h6);
+
+                String[] h7 = {"# Side of body: " + side_str};
+                writer.writeNext(h7);
+
+                String[] h8 = {"# Sensor mounting: " + mounting_str};
+                writer.writeNext(h8);
+
+                String[] h9 = {"# Number of steps: " + steps_str};
+                writer.writeNext(h9);
+
+                String[] h10 = {"# Notes: " + notes_str};
+                writer.writeNext(h10);
+
+                String[] h11 = {""};
+                writer.writeNext(h11);
+
+                String[] entries = "timestamp#seq#accel_x#accel_y#accel_z#gyro_x#gyro_y#gyro_z".split("#");
+                writer.writeNext(entries);
+
+                logging = true;
+                capture_started_timestamp = System.currentTimeMillis();
+                counter = 0;
+                Toast.makeText(this, "Start logging",
+                        Toast.LENGTH_SHORT).show();
+                stop_button.setEnabled(true);
             }
-
-            logging = true;
-            capture_started_timestamp = System.currentTimeMillis();
-            counter = 0;
-            Toast.makeText(this,"Start logging",
-                    Toast.LENGTH_SHORT).show();
-            stop_button.setEnabled(true);
         });
 
         stop_button.setOnClickListener(v-> {
@@ -192,7 +289,8 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                             // Process scan result here.
                             if (scanResult.getBleDevice().getMacAddress().equals(ORIENT_BLE_ADDRESS)) {
                                 runOnUiThread(() -> {
-                                    Toast.makeText(ctx, "Found " + scanResult.getBleDevice().getName(),
+                                    Toast.makeText(ctx, "Found " + scanResult.getBleDevice().getName() + ", " +
+                                                    scanResult.getBleDevice().getMacAddress(),
                                             Toast.LENGTH_SHORT).show();
                                 });
                                 connectToOrient(ORIENT_BLE_ADDRESS);
@@ -214,13 +312,19 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     public void onItemSelected(AdapterView<?> parent, View view, int position,
                                long id) {
         // TODO Auto-generated method stub
+        if (parent.getItemAtPosition(position).toString().compareTo("---") == 0) return;
+
         if (parent.getId() == groupSpinner.getId()) {
-            Toast.makeText(this, "GROUP : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
-            group = parent.getItemAtPosition(position).toString();
+            //Toast.makeText(this, "GROUP : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            group_str = parent.getItemAtPosition(position).toString();
         }
         else if (parent.getId() == positionSpinner.getId()) {
-            Toast.makeText(this, "POSITION : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
-            this.position = parent.getItemAtPosition(position).toString();
+            //Toast.makeText(this, "POSITION : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            this.position_str = parent.getItemAtPosition(position).toString();
+        }
+        else if (parent.getId() == activitySpinner.getId()) {
+            //Toast.makeText(this, "ACTIVITY : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            this.act_type_str = parent.getItemAtPosition(position).toString();
         }
     }
 
@@ -326,7 +430,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
             };
             writer.writeNext(entries);
 
-            if (counter % 25 == 0) {
+            if (counter % 10 == 0) {
                 long elapsed_time = System.currentTimeMillis() - capture_started_timestamp;
                 int total_secs = (int)elapsed_time / 1000;
                 int s = total_secs % 60;
@@ -344,8 +448,8 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
 
                 String time_str = m_str + ":" + s_str;
 
-                String accel_str = "Accel:(" + accel_x + ", " + accel_y + ", " + accel_z + ")";
-                String gyro_str = "Gyro:(" + gyro_x + ", " + gyro_y + ", " + gyro_z + ")";
+                String accel_str = "Accel: (" + accel_x + ", " + accel_y + ", " + accel_z + ")";
+                String gyro_str = "Gyro: (" + gyro_x + ", " + gyro_y + ", " + gyro_z + ")";
 
                 runOnUiThread(() -> {
                     captureTimetextView.setText(time_str);
