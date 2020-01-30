de74343fd4094fc3698132e623c6cdcb9fb77861
rat <rat@speckled>
2020-01-30 14:39:32 +0000

    basic service working

--- app/src/main/java/com/specknet/orientandroid/MainActivity.java
+++ app/src/main/java/com/specknet/orientandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.specknet.orientandroid;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -205,6 +206,9 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     }
 
     private void runApp() {
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
+
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
--- app/src/main/java/com/specknet/orientandroid/MyService.java
+++ app/src/main/java/com/specknet/orientandroid/MyService.java
@@ -1,16 +1,36 @@
 package com.specknet.orientandroid;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
+import android.util.Log;
 
-public class MyService extends Service {
+public class MyService extends IntentService {
+
+    /**
+     * A constructor is required, and must call the super <code><a href="/reference/android/app/IntentService.html#IntentService(java.lang.String)">IntentService(String)</a></code>
+     * constructor with a name for the worker thread.
+     */
     public MyService() {
+        super("MyService");
     }
 
+    /**
+     * The IntentService calls this method from the default worker thread with
+     * the intent that started the service. When this method returns, IntentService
+     * stops the service, as appropriate.
+     */
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    protected void onHandleIntent(Intent intent) {
+        // Normally we would do some work here, like download a file.
+        // For our sample, we just sleep for 5 seconds.
+        while(true) {
+            try {
+                Thread.sleep(100);
+                Log.i("MyService", "Hello");
+            } catch (InterruptedException e) {
+                // Restore interrupt status.
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
