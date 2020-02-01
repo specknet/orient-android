ad1b60d2fa7107fb7e2c24cd7e39c3369be10e11
rat <rat@speckled>
2020-02-01 09:25:24 +0000

    Runs at last

--- app/src/main/java/com/specknet/orientandroid/MyService2.java
+++ app/src/main/java/com/specknet/orientandroid/MyService2.java
@@ -1,11 +1,16 @@
 package com.specknet.orientandroid;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 /**
@@ -25,11 +30,20 @@ public class MyService2 extends Service  {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        startMyOwnForeground();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         new Thread() {
             @Override
             public void run() {
                 Log.i("MyService2", "Starting Sensor service...");
+
+                /*
+                //String channel = createChannel();
 
                 Intent notificationIntent = new Intent(MyService2.this, MainActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(MyService2.this, 0, notificationIntent, 0);
@@ -42,11 +56,31 @@ public class MyService2 extends Service  {
                         .build();
 
                 startForeground(SERVICE_NOTIFICATION_ID, notification);
+            */
             }
         }.start();
         return START_STICKY;
     }
 
+    private void startMyOwnForeground(){
+        String NOTIFICATION_CHANNEL_ID = "com.specknet.orientandroid";
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.vec_wireless_active)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+    }
 
     @Override
     public boolean onUnbind(Intent intent) {
