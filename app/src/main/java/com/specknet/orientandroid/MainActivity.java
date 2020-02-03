b45f94088ecb27d262f7b1e08e70ae58f19d3c04
rat <rat@speckled>
2020-02-03 19:46:01 +0000

    Revert "refactoring"
    This reverts commit 223c96cc11d18da6df8c9d67087977a4d7799181.

--- app/src/main/java/com/specknet/orientandroid/MainActivity.java
+++ app/src/main/java/com/specknet/orientandroid/MainActivity.java
@@ -61,7 +61,7 @@ import javax.vecmath.Vector3f;
 
 import static java.lang.Math.atan2;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     // test device - replace with the real BLE address of your sensor, which you can find
     // by scanning for devices with the NRF Connect App
@@ -90,17 +90,47 @@ public class MainActivity extends Activity {
     private DatagramPacket p;
 
 
+    //private int n = 0;
+    private Long connected_timestamp = null;
+    private Long capture_started_timestamp = null;
     boolean connected = false;
     private float freq = 0.f;
 
     private int counter = 0;
+    private CSVWriter writer;
+    private File path;
+    private File file;
+    private boolean logging = false;
 
-
-    private Button start_unity_button;
+    private Button start_button;
+    private Button stop_button;
     private Context ctx;
+    private TextView captureTimetextView;
     private TextView accelTextView;
     private TextView gyroTextView;
     private TextView freqTextView;
+    private EditText nameEditText;
+    private EditText notesEditText;
+
+    private Spinner positionSpinner;
+    private Spinner groupSpinner;
+    private Spinner activitySpinner;
+
+    private String group_str = null;
+    private String position_str = null;
+    private String act_type_str = null;
+    private String name_str = null;
+    private String notes_str = null;
+    private String side_str = null;
+
+    private String orientation_str = null;
+    private String steps_str = null;
+    private String mounting_str = null;
+
+    private RadioGroup sideRadioGroup;
+    private RadioGroup orientationRadioGroup;
+    private RadioGroup stepsRadioGroup;
+    private RadioGroup mountingRadioGroup;
 
     private final int RC_LOCATION_AND_STORAGE = 1;
 
@@ -149,30 +179,171 @@ public class MainActivity extends Activity {
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
-    public void startUnityGame(Context context) {
-        Intent intent = context.getPackageManager().getLaunchIntentForPackage("");
-
-        if (intent == null) {
-            Toast.makeText(context, "Can't find Unity teddybear visualisation app",
-                    Toast.LENGTH_LONG).show();
-        } else {
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(intent);
-        }
-    }
-
     private void runApp() {
-        accelTextView = findViewById(R.id.accelTextView);
-        gyroTextView = findViewById(R.id.gyroTextView);
-        freqTextView = findViewById(R.id.freqTextView);
 
+        //startDiaryApp(getActivity(), "com.specknet.rehabdiary");
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Intent intent = new Intent(this, MyService2.class);
         startService(intent);
 
-        start_unity_button.setOnClickListener(v-> {
-            startUnityGame(ctx);
+
+        path = Environment.getExternalStorageDirectory();
+
+        start_button = findViewById(R.id.start_button);
+        stop_button = findViewById(R.id.stop_button);
+        captureTimetextView = findViewById(R.id.captureTimetextView);
+        accelTextView = findViewById(R.id.accelTextView);
+        gyroTextView = findViewById(R.id.gyroTextView);
+        freqTextView = findViewById(R.id.freqTextView);
+        positionSpinner = findViewById(R.id.positionSpinner);
+        groupSpinner = findViewById(R.id.groupSpinner);
+        activitySpinner = findViewById(R.id.activitySpinner);
+        nameEditText = findViewById(R.id.nameEditText);
+        notesEditText = findViewById(R.id.notesEditText);
+
+        positionSpinner.setOnItemSelectedListener(this);
+        groupSpinner.setOnItemSelectedListener(this);
+        activitySpinner.setOnItemSelectedListener(this);
+
+        sideRadioGroup = findViewById(R.id.radioSide);
+        orientationRadioGroup = findViewById(R.id.radioSide);
+        stepsRadioGroup = findViewById(R.id.radioSteps);
+        mountingRadioGroup = findViewById(R.id.radioMouting);
+
+        List<String> position_list = new ArrayList<String>();
+        position_list.add("---");
+        position_list.add("Wrist");
+        position_list.add("Upper arm");
+        position_list.add("Torso");
+        position_list.add("Upper Leg");
+        position_list.add("Lower Leg");
+        position_list.add("Foot");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, position_list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        positionSpinner.setAdapter(adapter);
+
+        List<String> group_list = new ArrayList<String>();
+        group_list.add("---");
+        for (char c = 'A'; c <= 'Z'; c++){
+            group_list.add(String.valueOf(c));
+        }
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, group_list);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        groupSpinner.setAdapter(adapter2);
+
+        List<String> activity_list = new ArrayList<String>();
+        activity_list.add("---");
+        activity_list.add("Walking (level ground)");
+        activity_list.add("Climbing stairs");
+        activity_list.add("Descending stairs");
+        activity_list.add("Running");
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, activity_list);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        activitySpinner.setAdapter(adapter3);
+
+        start_button.setOnClickListener(v-> {
+
+            if (group_str == null || act_type_str == null || position_str == null || (nameEditText.getText().toString().length() == 0)) {
+                Toast.makeText(this,"Please complete all details",
+                        Toast.LENGTH_SHORT).show();
+            }
+            else {
+                start_button.setEnabled(false);
+
+                name_str = nameEditText.getText().toString();
+                notes_str = notesEditText.getText().toString();
+
+                int selectedId;
+                RadioButton rb;
+
+                selectedId = sideRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                side_str = rb.getText().toString();
+
+                selectedId = orientationRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                orientation_str = rb.getText().toString();
+
+                selectedId = stepsRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                steps_str = rb.getText().toString();
+
+                selectedId = mountingRadioGroup.getCheckedRadioButtonId();
+                rb = findViewById(selectedId);
+                mounting_str = rb.getText().toString();
+
+
+                // make a new filename based on the start timestamp
+                String file_ts = new SimpleDateFormat("yyyyMMdd'T'HHmmss").format(new Date());
+                file = new File(path, "PDIoT_" + group_str + "_" + file_ts + ".csv");
+
+                try {
+                    writer = new CSVWriter(new FileWriter(file), ',');
+                } catch (IOException e) {
+                    Log.e("MainActivity", "Caught IOException: " + e.getMessage());
+                }
+
+                String[] h1 = {"# PDIoT walking data"};
+                writer.writeNext(h1);
+
+                String[] h2 = {"# Start time: " + file_ts};
+                writer.writeNext(h2);
+
+                String[] h3 = {"# Group: " + group_str};
+                writer.writeNext(h3);
+
+                String[] h4 = {"# Subject: " + name_str};
+                writer.writeNext(h4);
+
+                String[] h5 = {"# Activity type: " + act_type_str};
+                writer.writeNext(h5);
+
+                String[] h6 = {"# Sensor position: " + position_str};
+                writer.writeNext(h6);
+
+                String[] h7 = {"# Side of body: " + side_str};
+                writer.writeNext(h7);
+
+                String[] h8 = {"# Sensor mounting: " + mounting_str};
+                writer.writeNext(h8);
+
+                String[] h9 = {"# Number of steps: " + steps_str};
+                writer.writeNext(h9);
+
+                String[] h10 = {"# Notes: " + notes_str};
+                writer.writeNext(h10);
+
+                String[] h11 = {""};
+                writer.writeNext(h11);
+
+                String[] entries = "timestamp#seq#accel_x#accel_y#accel_z#gyro_x#gyro_y#gyro_z#mag_x#mag_y#mag_z".split("#");
+                writer.writeNext(entries);
+
+                logging = true;
+                capture_started_timestamp = System.currentTimeMillis();
+                counter = 0;
+                Toast.makeText(this, "Start logging",
+                        Toast.LENGTH_SHORT).show();
+                stop_button.setEnabled(true);
+            }
+        });
+
+        stop_button.setOnClickListener(v-> {
+            logging = false;
+            stop_button.setEnabled(false);
+            try {
+                writer.flush();
+                writer.close();
+                Toast.makeText(this,"Recording saved",
+                        Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                Log.e("MainActivity", "Caught IOException: " + e.getMessage());
+            }
+            start_button.setEnabled(true);
         });
 
         port = UDP_PORT;
@@ -237,6 +408,30 @@ public class MainActivity extends Activity {
                 );
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+        // TODO Auto-generated method stub
+        if (parent.getItemAtPosition(position).toString().compareTo("---") == 0) return;
+
+        if (parent.getId() == groupSpinner.getId()) {
+            //Toast.makeText(this, "GROUP : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            group_str = parent.getItemAtPosition(position).toString();
+        }
+        else if (parent.getId() == positionSpinner.getId()) {
+            //Toast.makeText(this, "POSITION : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            this.position_str = parent.getItemAtPosition(position).toString();
+        }
+        else if (parent.getId() == activitySpinner.getId()) {
+            //Toast.makeText(this, "ACTIVITY : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            this.act_type_str = parent.getItemAtPosition(position).toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
 
     private void connectToOrient(String addr) {
         orient_device = rxBleClient.getBleDevice(addr);
@@ -257,11 +452,11 @@ public class MainActivity extends Activity {
                             //Log.i("OrientAndroid", "Received " + bytes.length + " bytes");
                             if (!connected) {
                                 connected = true;
-                                //connected_timestamp = System.currentTimeMillis();
+                                connected_timestamp = System.currentTimeMillis();
                                 runOnUiThread(() -> {
                                                                Toast.makeText(ctx, "Receiving sensor data",
                                                                        Toast.LENGTH_SHORT).show();
-                                    start_unity_button.setEnabled(true);
+                                    start_button.setEnabled(true);
                                                            });
                             }
                             if (raw) handleRawPacket(bytes); else handleQuatPacket(bytes);
@@ -364,23 +559,60 @@ public class MainActivity extends Activity {
             }
         }
 
+        if (logging) {
+            //String[] entries = "first#second#third".split("#");
+            String[] entries = {Long.toString(ts),
+                    Integer.toString(counter),
+                    Float.toString(accel_x),
+                    Float.toString(accel_y),
+                    Float.toString(accel_z),
+                    Float.toString(gyro_x),
+                    Float.toString(gyro_y),
+                    Float.toString(gyro_z),
+                    Float.toString(mag_x),
+                    Float.toString(mag_y),
+                    Float.toString(mag_z),
+            };
+            writer.writeNext(entries);
+
+            if (counter % 12 == 0) {
+                long elapsed_time = System.currentTimeMillis() - capture_started_timestamp;
+                int total_secs = (int)elapsed_time / 1000;
+                int s = total_secs % 60;
+                int m = total_secs / 60;
+
+                String m_str = Integer.toString(m);
+                if (m_str.length() < 2) {
+                    m_str = "0" + m_str;
+                }
+
+                String s_str = Integer.toString(s);
+                if (s_str.length() < 2) {
+                    s_str = "0" + s_str;
+                }
 
 
-        if (counter % 12 == 0) {
+                Long elapsed_capture_time = System.currentTimeMillis() - capture_started_timestamp;
+                float connected_secs = elapsed_capture_time / 1000.f;
+                freq = counter / connected_secs;
+                //Log.i("OrientAndroid", "Packet count: " + Integer.toString(n) + ", Freq: " + Float.toString(freq));
 
-            String accel_str = "Accel: (" + accel_x + ", " + accel_y + ", " + accel_z + ")";
-            String gyro_str = "Gyro: (" + gyro_x + ", " + gyro_y + ", " + gyro_z + ")";
-            String freq_str = "Freq: " + freq;
+                String time_str = m_str + ":" + s_str;
 
-            runOnUiThread(() -> {
-                accelTextView.setText(accel_str);
-                gyroTextView.setText(gyro_str);
-                freqTextView.setText(freq_str);
-            });
+                String accel_str = "Accel: (" + accel_x + ", " + accel_y + ", " + accel_z + ")";
+                String gyro_str = "Gyro: (" + gyro_x + ", " + gyro_y + ", " + gyro_z + ")";
+                String freq_str = "Freq: " + freq;
+
+                runOnUiThread(() -> {
+                    captureTimetextView.setText(time_str);
+                    accelTextView.setText(accel_str);
+                    gyroTextView.setText(gyro_str);
+                    freqTextView.setText(freq_str);
+                });
+            }
+
+            counter += 1;
         }
-
-        counter += 1;
-
     }
 
     @Override
